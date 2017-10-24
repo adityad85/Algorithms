32e344012a96f66ff11d8d18edb16105089993cf
adityad85 <aditya85d@gmail.com>
2017-10-24 16:23:51 +0530

    insertion sort
    in process but working ,details left

---
+++ Sorting/Insertion_Sort.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+int mpow(int base, int exp);
+void ipgraph(int m);
+const int mod = 1000000007;
+//=======================
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	vl asq;
+	long long n,i,j,q;
+	cin>>n;
+	for(int i=0;cin>>q;){
+        asq.pb(q);
+	}
+	fo(i,n){
+	    int k=asq[i];
+	    j=i-1;
+	    while(j>=0&&k<asq[j]){
+            asq[j+1]=asq[j];
+            j--;
+	    }
+	    asq[j+1]=k;
+	}
+	fo(i,n){
+	cout<<asq[i]<<" ";}
+
+	return 0;
+}
+
