fbde50fa58d3b22c42ce20b544d26ef44f034772
adityad85 <aditya85d@gmail.com>
2018-01-09 16:29:20 +0530

    done

---
+++ Data Structures/Order Statistics.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> // Common file
+#include <ext/pb_ds/tree_policy.hpp>  // including tree_order_statistics_node_update
+// #include "stdafx.h"
+// #pragma warning(disable : 4996) //_CRT_SECURE_NO_WARNINGS
+using namespace std;
+using namespace __gnu_pbds;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define ll long long
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+int mpow(int base, int exp);
+void ipgraph(int m);
+const int mod = 1000000007;
+//=======================
+typedef tree <int , null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>pbds;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+    pbds x;
+    x.insert(1);
+    x.insert(3);
+    x.insert(4);// insert the number
+    x.erase(3); // delete the number
+    int k;
+    cin>>k;
+    cout<<*x.find_by_order(k)<<"\n"; //find the number present at this point
+    int a;
+    cin >>a;
+    cout<<*x.upper_bound(a)<<"\n"; //find the number after the given number
+	return 0;
+}
+
